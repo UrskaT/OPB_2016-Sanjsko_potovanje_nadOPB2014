b9b9a2edab4b60d415580a286a855a302d136e5b
Urška Tomšič <urska.tomsic@gmail.com>
2016-06-13 13:38:11 +0200

    Popravki sheme, napačna sintaksa

--- 2. Baza in podatki/0. Sanjsko_potovanje_BAZA_SQLshema.sql
+++ 2. Baza in podatki/0. Sanjsko_potovanje_BAZA_SQLshema.sql
@@ -3,22 +3,22 @@ CREATE TABLE potnik(
 	ime TEXT NOT NULL,
 	priimek TEXT NOT NULL,
 	rojstni_datum DATE NOT NULL,
-	placilna_kartica INTEGER NOT NULL,
+	placilna_kartica INTEGER NOT NULL
 );
 	
 CREATE TABLE lokacija (
 	id INTEGER PRIMARY KEY,
 	mesto TEXT NOT NULL UNIQUE,
-	drzava TEXT NOT NULL,
+	drzava TEXT NOT NULL
 );	
 	
 CREATE TABLE zelja(
 	id_st SERIAL PRIMARY KEY,
-	lastnik INTEGER REFERENCES potnik(emso),
+	lastnik INTEGER REFERENCES potnik(emso)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
 	zacetna_lokacija INTEGER NOT NULL REFERENCES lokacija(id)
-		ON DELETE CASCADE
+		ON DELETE CASCADE,
 	zeli_iti INTEGER NOT NULL REFERENCES lokacija(id)
 		ON DELETE CASCADE
 );
@@ -42,15 +42,15 @@ CREATE TABLE ponudnik(
 
 CREATE TABLE let(
 	 id_let SERIAL PRIMARY KEY,
-	 letalska_druzba TEXT NOT NULL REFERENCES ponudnik(ime),
+	 letalska_druzba INTEGER NOT NULL REFERENCES ponudnik(id_ponud)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE,
-	kam_leti INTEGER NOT NULL,
-	od_kod INTEGER NOT NULL,
-	FOREIGN KEY (kam_leti, od_kod)
-		REFERENCES letalisce(ime)
-			ON DELETE RESTRICT
-			ON UPDATE CASCADE,
+	kam_leti INTEGER NOT NULL REFERENCES letalisce(id_air)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE,
+	od_kod INTEGER NOT NULL REFERENCES letalisce(id_air)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE,
 	dolzina FLOAT NOT NULL,
 	cena FLOAT NOT NULL
 );
@@ -59,26 +59,10 @@ CREATE TABLE let(
 
 CREATE TABLE karta(
 	id_kart SERIAL PRIMARY KEY,
-	kupec INTEGER REFERENCES potnik(ime),
-	cena_leta FLOAT NOT NULL,
-	zacetno_letalisce TEXT NOT NULL,
-	koncno_letalisce TEXT NOT NULL,
-	nudi_let INTEGER NOT NULL,
-	FOREIGN KEY (cena_leta, zacetno_letalisce, koncno_letalisce, nudi_let)
-		REFERENCES let(cena, od_kod, kam_leti, letalska_druzba)
-			ON DELETE RESTRICT
-			ON UPDATE RESTRICT,
-	zacetna_destinacija TEXT NOT NULL,
-	koncna_destinacija TEXT NOT NULL, 
-	FOREIGN KEY (zacetna_destinacija, koncna_destinacija)
-		REFERENCES lokacija(id, id)
+	kupec INTEGER REFERENCES potnik(emso),
+	polet INTEGER NOT NULL REFERENCES let(id_let)
 			ON DELETE RESTRICT
 			ON UPDATE RESTRICT
 );
 	
 -- Karta se izbriše (premakne v arhiv) ko je unovčena, ima rok uporabe...
-	
-	
-	
-	
-	
