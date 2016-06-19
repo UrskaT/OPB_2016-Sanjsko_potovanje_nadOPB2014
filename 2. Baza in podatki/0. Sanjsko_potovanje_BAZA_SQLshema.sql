0956e4903de6cf9bb3e6c364ae4bb5352a5d7f0a
Urška Tomšič <urska.tomsic@gmail.com>
2016-06-19 23:06:04 +0200

    Popravki za login, uporabnisko_ime

--- 2. Baza in podatki/0. Sanjsko_potovanje_BAZA_SQLshema.sql
+++ 2. Baza in podatki/0. Sanjsko_potovanje_BAZA_SQLshema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE potnik(
-	emso INTEGER PRIMARY KEY,
+	uporabnisko_ime TEXT PRIMARY KEY,
+	geslo text NOT NULL,
 	ime TEXT NOT NULL,
 	priimek TEXT NOT NULL,
-	rojstni_datum DATE NOT NULL,
 	placilna_kartica INTEGER NOT NULL
 );
 	
@@ -14,7 +14,7 @@ CREATE TABLE lokacija (
 	
 CREATE TABLE zelja(
 	id_st SERIAL PRIMARY KEY,
-	lastnik INTEGER REFERENCES potnik(emso)
+	lastnik TEXT REFERENCES potnik(uporabnisko_ime)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
 	zacetna_lokacija INTEGER NOT NULL REFERENCES lokacija(id)
@@ -27,7 +27,7 @@ CREATE TABLE zelja(
 CREATE TABLE letalisce (	
 	id_air INTEGER PRIMARY KEY,
 	ime_letalisca TEXT NOT NULL,
-	bliznje INTEGER NOT NULL REFERENCES lokacija(id)
+	blizinje INTEGER NOT NULL REFERENCES lokacija(id)
 		ON DELETE RESTRICT
 		ON UPDATE RESTRICT,
 	gps_sirina FLOAT NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE letalisce (
 
 CREATE TABLE ponudnik(
 	id_ponud INTEGER PRIMARY KEY,
-	ime_ponudnika TEXT NOT NULL,
+	ime TEXT NOT NULL,
 	cenovni_razred FLOAT NOT NULL
 );
 
@@ -59,10 +59,21 @@ CREATE TABLE let(
 
 CREATE TABLE karta(
 	id_kart SERIAL PRIMARY KEY,
-	kupec INTEGER REFERENCES potnik(emso),
+	kupec TEXT REFERENCES potnik(uporabnisko_ime),
 	polet INTEGER NOT NULL REFERENCES let(id_let)
 			ON DELETE RESTRICT
 			ON UPDATE RESTRICT
 );
 	
 -- Karta se izbriše (premakne v arhiv) ko je unovčena, ima rok uporabe...
+
+/* Podatki izpisani na karti:
+	cena_leta FLOAT NOT NULL,
+	zacetno_letalisce TEXT NOT NULL,
+	koncno_letalisce TEXT NOT NULL,
+	nudi_let INTEGER NOT NULL,
+	zacetna_destinacija TEXT NOT NULL,
+	koncna_destinacija TEXT NOT NULL, 
+	id_karte
+	kupec
+*/
