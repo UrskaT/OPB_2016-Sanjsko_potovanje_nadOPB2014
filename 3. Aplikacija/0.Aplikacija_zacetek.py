105b8d8d08249d57b0a3010f3dd5e2abe0cf5042
Urška Tomšič <urska.tomsic@gmail.com>
2016-06-20 11:31:47 +0200

    Začetek izdelave spletne strani

---
+++ 3. Aplikacija/0.Aplikacija_zacetek.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import auth_public as auth
+import bottle
+import hashlib
+
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+
+# odkomentiraj, če želiš sporočila o napakah
+# debug(True)
+
+# Mapa s statičnimi datotekami
+static_dir = = "./static"
+######################################################################
+# POMOŽNE FUNKCIJE
+
+def password_md5(s):
+    """Vrni MD5 hash danega UTF-8 niza. Gesla vedno spravimo v bazo
+       kodirana s to funkcijo."""
+    h = hashlib.md5()
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
+
+# Funkcija, ki v cookie spravi sporocilo
+def set_sporocilo(tip, vsebina):
+    bottle.response.set_cookie('message', (tip, vsebina), path='/', secret=secret)
+
+# Funkcija, ki iz cookija dobi sporočilo, če je
+def get_sporocilo():
+    sporocilo = bottle.request.get_cookie('message', default=None, secret=secret)
+    bottle.response.delete_cookie('message')
+    return sporocilo
+
+def get_user(auto_login = True):
+    """Poglej cookie in ugotovi, kdo je prijavljeni uporabnik,
+       vrni njegov username in ime. Če ni prijavljen, presumeri
+       na stran za prijavo ali vrni None (advisno od auto_login).
+    """
+    # Dobimo username iz piškotka
+    username = bottle.request.get_cookie('username', secret=secret)
+    # Preverimo, ali ta uporabnik obstaja
+    if username is not None:
+        ## ???? c = baza.cursor()
+        c.execute("SELECT uporabnisko_ime, ime, priimek FROM potnik WHERE username=%s",
+                  [username])
+        r = c.fetchone()
+        ## ???? c.close ()
+        if r is not None:
+            # uporabnik obstaja, vrnemo njegove podatke
+            return r
+    # Če pridemo do sem, uporabnik ni prijavljen, naredimo redirect
+    if auto_login:
+        bottle.redirect('/login/')
+    else:
+        return None
+
+######################################################################
+# FUNKCIJE, KI OBDELAJO ZAHTEVE ODJEMALCEV
+
+@bottle.route("/static/<filename:path>")
+def static(filename):
+    """Splošna funkcija, ki servira vse statične datoteke iz naslova
+       /static/..."""
+    return bottle.static_file(filename, root=static_dir)
+
+@bottle.get("/login/")
+def login_get():
+    """Serviraj formo za login."""
+    return bottle.template("login.html",
+                           napaka=None,
+                           username=None)
+
+@bottle.post("/login/")
+def login_post():
+    """Obdelaj izpolnjeno formo za prijavo"""
+    # Uporabniško ime, ki ga je uporabnik vpisal v formo
+    username = bottle.request.forms.username
+    # Izračunamo MD5 has gesla, ki ga bomo spravili
+    password = password_md5(bottle.request.forms.password)
+    # Preverimo, ali se je uporabnik pravilno prijavil
+    ## ???? c = baza.cursor() ????tuki pa ni c.close????
+    c.execute("SELECT 1 FROM potnik WHERE uporabnisko_ime=%s AND geslo=%s",
+              [username, password])
+    if c.fetchone() is None:
+        # Username in geslo se ne ujemata
+        return bottle.template("login.html",
+                               napaka="Nepravilna prijava",
+                               username=username)
+    else:
+        # Vse je v redu, nastavimo cookie in preusmerimo na glavno stran
+        bottle.response.set_cookie('username', username, path='/', secret=secret)
+        bottle.redirect("/")
+
+@bottle.get("/register/")
+def login_get():
+    """Prikaži formo za registracijo."""
+    return bottle.template("register.html", 
+                           username=None,
+                           ime=None,
+                           napaka=None)
+
+@bottle.post("/register/")
+def register_post():
+    """Registriraj novega uporabnika."""
+    username = bottle.request.forms.username
+    ime = bottle.request.forms.ime
+    password1 = bottle.request.forms.password1
+    password2 = bottle.request.forms.password2
+    # Ali uporabnik že obstaja?
+    ## ???? c = baza.cursor()
+    c.execute("SELECT 1 FROM potnik WHERE uporabnisko_ime=%s", [username])
+    if c.fetchone():
+        # Uporabnik že obstaja
+        return bottle.template("register.html",
+                               username=username,
+                               ime=ime,
+                               napaka='To uporabniško ime je že zavzeto')
+    elif not password1 == password2:
+        # Geslo se ne ujemata
+        return bottle.template("register.html",
+                               username=username,
+                               ime=ime,
+                               napaka='Gesli se ne ujemata')
+    else:
+        # Vse je v redu, vstavi novega uporabnika v bazo
+        password = password_md5(password1)
+        c.execute("INSERT INTO potnik (uporabnisko_ime, ime, geslo) VALUES (%s, %s, %s",
+                  (username, ime, password))
+        # Daj uporabniku cookie
+        bottle.response.set_cookie('username', username, path='/', secret=secret)
+        bottle.redirect("/")
+
+@bottle.get("/logout/")
+def logout():
+    """Pobriši cookie in preusmeri na login."""
+    bottle.response.delete_cookie('username')
+    bottle.redirect('/login/')
+
+
+
+######################################################################
+# Glavni program
+
+# priklopimo se na bazo
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+c = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+
+# poženemo strežnik na portu 8080, glej http://localhost:8080/
+run(host='localhost', port=8080, reloader=True, debug=True)
+
+## ? debug=True ?
+## že zgori bla ena možnost...
+
+
